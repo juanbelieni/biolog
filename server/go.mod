ec8da1bdea3447524f5f86067722d0a9f05998f9
Juan Belieni <juanbelieni@gmail.com>
2020-08-24 17:59:07 -0300

    Add .env in server

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
--- server/main.go
+++ server/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/juanbelieni/biolog/server/app"
+import (
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/juanbelieni/biolog/server/app"
+)
 
 func main() {
 	app := app.App
-	app.Logger.Fatal(app.Start(":3333"))
+	app.Logger.Fatal(app.Start(":" + os.Getenv("PORT")))
 }
