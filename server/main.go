0fb2a318323dc884bdbfd8596f4c8dc901989581
Juan Belieni <juanbelieni@gmail.com>
2020-09-03 10:48:04 -0300

    Remove port from .env and delete deploy action

--- server/main.go
+++ server/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"os"
-
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/juanbelieni/biolog/server/app"
 )
 
 func main() {
-	var port string
 	app := app.App
-
-	port, exists := os.LookupEnv("PORT")
-	if !exists {
-		port = "3333"
-	}
+	port = "3333"
 
 	app.Logger.Fatal(app.Start(":" + port))
 }
