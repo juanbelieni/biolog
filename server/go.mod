5701dddb4e876ea2f2d93a437c41b537442a6d4d
Juan Belieni <juanbelieni@gmail.com>
2020-09-07 18:59:31 -0300

    Last try

--- server/go.mod
+++ server/go.mod
@@ -3,7 +3,7 @@ module github.com/juanbelieni/biolog/server
 go 1.14
 
 require (
-	// cloud.google.com/go/storage v1.11.0
+	cloud.google.com/go/storage v1.11.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/jackc/pgproto3/v2 v2.0.4 // indirect
 	github.com/joho/godotenv v1.3.0
@@ -12,8 +12,8 @@ require (
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
 	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	// google.golang.org/api v0.31.0
-	// google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d // indirect
+	google.golang.org/api v0.31.0
+	google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d // indirect
 	gorm.io/driver/postgres v1.0.0
 	gorm.io/gorm v1.20.0
 )
--- server/utils/upload-file.go
+++ server/utils/upload-file.go
@@ -1,30 +1,33 @@
 package utils
 
 import (
+	"context"
+	"io"
 	"mime/multipart"
-	// "cloud.google.com/go/storage"
-	// "google.golang.org/api/option"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
 )
 
 func UploadFile(file multipart.File, filename string) (string, error) {
-	// ctx := context.Background()
+	ctx := context.Background()
 
-	// storageClient, err := storage.NewClient(ctx, option.WithCredentialsFile("google-credentials.json"))
-	// if err != nil {
-	// 	return "", err
-	// }
+	storageClient, err := storage.NewClient(ctx, option.WithCredentialsFile("google-credentials.json"))
+	if err != nil {
+		return "", err
+	}
 
-	// sw := storageClient.Bucket("biolog").Object(filename).NewWriter(ctx)
+	sw := storageClient.Bucket("biolog").Object(filename).NewWriter(ctx)
 
-	// _, err = io.Copy(sw, file)
-	// if err != nil {
-	// 	return "", err
-	// }
+	_, err = io.Copy(sw, file)
+	if err != nil {
+		return "", err
+	}
 
-	// err = sw.Close()
-	// if err != nil {
-	// 	return "", err
-	// }
+	err = sw.Close()
+	if err != nil {
+		return "", err
+	}
 
 	return "https://storage.googleapis.com/biolog/" + filename, nil
 }
