c45df2bdb0a7e8a02803c91deb0a0b22279da09d
Juan Belieni <juanbelieni@gmail.com>
2020-10-22 14:00:49 -0300

    Add image to Home Page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// import BorboletaImg from '../assets/borboleta.png';
 import Button from '../components/Button';
 import Page from '../components/Page';
 import styles from './index.module.scss';
@@ -8,18 +9,19 @@ export default function Home() {
   return (
     <Page>
       <section className={styles.aboutUs}>
-        <h1 className={styles.title}>
-          Espécies de <br />
-          <b>borboletas</b> em <br />
-          Minas Gerais
-        </h1>
-
-        <p className={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-
-        <Button>Leia mais</Button>
+        <div>
+          <h1 className={styles.title}>
+            Espécies de <br />
+            <b>borboletas</b> em <br />
+            Minas Gerais
+          </h1>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Button>Leia mais</Button>
+        </div>
+        <img src="/borboleta.png" />
       </section>
     </Page>
   );
